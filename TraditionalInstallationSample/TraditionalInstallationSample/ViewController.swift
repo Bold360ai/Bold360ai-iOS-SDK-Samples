3ffc4f810377b30bcde29a3b5f5f02b09adc8db2
elizasapir <eliza.koren@logmein.com>
2018-09-16 11:40:51 +0300

    load nanorep, loads controller

--- TraditionalInstallationSample/TraditionalInstallationSample/ViewController.swift
+++ TraditionalInstallationSample/TraditionalInstallationSample/ViewController.swift
@@ -9,14 +9,34 @@ import NanorepUI
 
 class ViewController: UIViewController {
     
+    private let accountParams = AccountParams()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        self.accountParams.account = ""
+        self.accountParams.knowledgeBase = ""
+        self.accountParams.apiKey = ""
+    }
+    
+    var controller: NRConversationalViewController!
+    
+    
     @IBAction func loadNanorep(_ sender: UIButton) {
-        // Do your staff
         print("bold360ai sdk is loaded")
+        
+        // Set config
+        let config: NRBotConfiguration = NRBotConfiguration()
+        config.chatContentURL = URL(string:"")
+        config.withNavBar = true
+        // Create NRConversationalViewController
+        controller = NRConversationalViewController(accountParams: self.accountParams)
+        controller.configuration = config
+        
+        self.present(controller, animated: true, completion: nil)
     }
 }
 
