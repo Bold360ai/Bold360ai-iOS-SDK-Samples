884b5b8fa78bcfa96f02228f4aa2545e733efa92
Nissim Pardo <appdeveloper@mrplay.partners.com>
2019-01-07 15:39:10 +0200

    Added tab bar for bold/bot

---
+++ Conversation-Samples/HistorySample/HistorySample/BoldViewController.swift
@@ -0,0 +1,44 @@
+//
+//  BoldViewController.swift
+//  HistorySample
+//
+//  Created by Nissim Pardo on 07/01/2019.
+//  Copyright © 2019 bold360ai. All rights reserved.
+//
+
+import UIKit
+import BoldUI
+
+class BoldViewController: UIViewController {
+    var chatController: NRChatController!
+
+    @IBOutlet weak var accessKey: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func presentBold(_ sender: UIBarButtonItem) {
+        let account = BCAccount(accessKey: self.accessKey.text)
+        self.chatController = NRChatController(account: account)
+        self.chatController.delegate = self
+    }
+
+}
+
+extension BoldViewController: NRChatControllerDelegate {
+    func shouldPresentChatViewController(_ viewController: UIViewController!) {
+        self.navigationController?.pushViewController(viewController, animated: true)
+    }
+    
+    func didFailLoadChatWithError(_ error: Error!) {
+        
+    }
+}
